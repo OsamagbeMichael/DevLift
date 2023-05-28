38c65f1f15da7d7a9680935a0e1133045dcf1c2b
OsamagbeMichael <uosamagbe@Gmail.com>
2023-05-28 11:59:56 -0400

    .

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -35,7 +35,7 @@ const Login: React.FC = () => {
         return true;
       })
       .catch(async (error) => {
-        const errorCode = error.code;
+        // const errorCode = error.code;
         const errorMessage = error.message;
 
         await alert({
